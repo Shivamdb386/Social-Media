8671cd8f6da72dd7c397b5939ba04ddab3c61092
Shivam Dubey <shivam.dubey2@ibm.com>
2024-07-13 14:03:33 +0530

    Uploading

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Sidebar from './Sidebar';
+import Footer from './Footer';
+import Posts from './Posts';
+import dog from './dog.png';
+import dog1 from './dog1.png';
+import { useState } from 'react';
 
 function App() {
+  // const [tab,settab] = useState["Home"];
+  let Postdetails= [{dogimage : dog,dogname : "Lebrador",dogft : "This dog is known for its smartness"},
+        {dogimage : dog1,dogname : "Rottweiler",dogft : "This dog is known for its Strength"}];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sidebar></Sidebar>
+      <div className='navdiv'> 
+      <Navbar></Navbar>
+      <Posts Postdetail = {Postdetails[0]}></Posts>
+      <Posts Postdetail = {Postdetails[1]}></Posts>
+      <Footer></Footer>
+      </div>
     </div>
   );
 }
---
+++ src/Posts.jsx
@@ -0,0 +1,24 @@
+import App from "./App";
+function Posts({Postdetail}){
+    function handledetails(dgname){
+        console.log(dgname);
+        if(dgname === "Lebrador")
+        window.open("https://www.akc.org/dog-breeds/labrador-retriever/","_blank", "noreferrer");
+        else if(dgname ===  "Rottweiler")
+        window.open("https://www.akc.org/dog-breeds/rottweiler/");
+
+
+    }
+
+    return(
+        <div className="card posts" style={{width: '18rem'}}>
+        <img class="card-img-top" src={Postdetail.dogimage} alt="Card image cap"/>
+        <div class="card-body">
+         <h5 class="card-title">{Postdetail.dogname}</h5>
+          <p class="card-text">{Postdetail.dogft}</p>
+          <a href="#" class="btn btn-primary" onClick={handledetails(Postdetail.dogname)}>More Details</a>
+           </div>
+           </div>
+    );
+}
+export default Posts
